cde5f4e3779256134bb4292f6e593faabeb14f77
jvcl <valdi.jorge@gmail.com>
2015-03-28 16:58:25 +1030

    get path from the intent

--- app/src/main/java/au/com/innovus/labelmatching/ResultActivity.java
+++ app/src/main/java/au/com/innovus/labelmatching/ResultActivity.java
@@ -1,6 +1,7 @@
 package au.com.innovus.labelmatching;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +13,8 @@ public class ResultActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        String filePath = getIntent().getStringExtra("filePath");
+
     }
 
 
