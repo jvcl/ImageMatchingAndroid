cee6fd766dca2431a2aa4465d8806d7c88a9a21a
jvcl <valdi.jorge@gmail.com>
2015-03-28 14:38:19 +1030

    add click listener

--- app/src/main/java/au/com/innovus/labelmatching/MainActivity.java
+++ app/src/main/java/au/com/innovus/labelmatching/MainActivity.java
@@ -4,14 +4,17 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_match).setOnClickListener(this);
     }
 
 
@@ -35,5 +38,10 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
